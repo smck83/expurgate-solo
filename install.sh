571668e1d0934996feff38b7a84287116014a2be
smck83 <s@mck.la>
2023-06-26 14:27:27 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# install script for standalone debian host, or Amazon Lightsail Debian instance.
+
+
 apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends apt-transport-https ca-certificates curl gnupg2 software-properties-common && \
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && \
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list && \
